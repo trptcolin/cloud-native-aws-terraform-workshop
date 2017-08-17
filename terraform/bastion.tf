596176b3808d5aaf60a1cce80599253dbb2776aa
Colin Jones <trptcolin@gmail.com>
2017-08-17 16:44:52 -0500

    Delete terraform stuff

--- terraform/bastion.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "bastion" {
-  ami = "ami-c481fad3"
-  subnet_id = "${aws_subnet.public_subnet_1.id}"
-  associate_public_ip_address = true
-  key_name = "colin-mbp-touchbar"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.bastion_sg.id}"]
-}
